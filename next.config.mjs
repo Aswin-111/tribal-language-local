8e77a954620c66c20d5b1ffbbaa448ae94bea5d5
Aswin <aswindkumar764@gmail.com>
2024-11-19 12:52:54 +0530

    build path fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     distDir: process.env.NEXT_PUBLIC_DIST // Specify your custom build directory here
-};
-
-module.exports = nextConfig;
+  };
+  
+  export default nextConfig;
